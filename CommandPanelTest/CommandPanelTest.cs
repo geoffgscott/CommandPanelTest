35d0b5265696f85fb6dfdb0144018654fd605b03
Geoff Scott <geoffgscott@gmail.com>
2023-07-07 14:43:03 -0700

    feat: Minimal reproduction for missing commands

---
+++ CommandPanelTest/CommandPanelTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Xarial.XCad.Base.Attributes;
+using Xarial.XCad.SolidWorks;
+using Xarial.XCad.UI.Commands;
+using System.Runtime.InteropServices;
+using CommandPanelTest.Properties;
+using System.Resources;
+
+using Xarial.XCad.UI.Commands.Attributes;
+using Xarial.XCad.UI.Commands.Enums;
+
+namespace CommandPanelTest
+{
+    [ComVisible(true)]
+    [Guid("D370C945-98D0-46DD-A04B-FCB7D3C15D41")]
+    public class CommandIconsSwAddIn : SwAddInEx
+    {
+        [Title("Power Tools")]
+        public enum Commands_e
+        {
+            [CommandItemInfo(WorkspaceTypes_e.AllDocuments)]
+            [Icon(typeof(Resources), nameof(Resources.electric_drill))]
+            Drill,
+            [Icon(typeof(Resources), nameof(Resources.electric_screwdriver))]
+            Screwdriver,
+            [Icon(typeof(Resources), nameof(Resources.power_saw))]
+            Saw
+        }
+
+        public override void OnConnect()
+        {
+            CommandManager.AddCommandGroup<Commands_e>().CommandClick += OnButtonClick;
+        }
+
+        private void OnButtonClick(Commands_e cmd)
+        {
+            switch (cmd)
+            {
+                case Commands_e.Drill:
+                    {
+                        Application.ShowMessageBox("Drill buttons is clicked");
+                        break;
+                    }
+
+                case Commands_e.Screwdriver:
+                    {
+                        Application.ShowMessageBox("Screwdriver buttons is clicked");
+                        break;
+                    }
+
+                case Commands_e.Saw:
+                    {
+                        Application.ShowMessageBox("Saw buttons is clicked");
+                        break;
+                    }
+            }
+        }
+    }
+}
